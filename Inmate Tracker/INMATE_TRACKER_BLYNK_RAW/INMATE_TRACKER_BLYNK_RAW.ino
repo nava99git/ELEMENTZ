ae41d55c236df2b0206b41d84872d5cfdebee56e
Navaneeth Krishnan <navaneeth328krishnan@gmail.com>
2021-06-19 19:42:29 +0530

    Final update for Inamate Tracker

--- Inmate Tracker/INMATE_TRACKER_BLYNK_RAW/INMATE_TRACKER_BLYNK_RAW.ino
+++ Inmate Tracker/INMATE_TRACKER_BLYNK_RAW/INMATE_TRACKER_BLYNK_RAW.ino
@@ -1,19 +1,14 @@
-/* This the code for the Inmate Tracker Project*/
-/* Comment this out to disable prints and save space */
 #define BLYNK_PRINT Serial
 
-// Define your modem:
 #define TINY_GSM_MODEM_SIM808
 
-// Default heartbeat interval for GSM is 60
-// If you want override this value, uncomment and set this option:
 #define BLYNK_HEARTBEAT 8
 
 #include <TinyGsmClient.h>
 #include <BlynkSimpleTinyGSM.h>
 
-// Blynk AUTH Token
-char auth[] = "INuDSNdD9Gb7DX3PjWMn-QfjW3SaYy3Q";
+//char auth[] = "INuDSNdD9Gb7DX3PjWMn-QfjW3SaYy3Q";
+char auth[]  = "Z1vgcUNL8-MoM96w2Cx_4-KZdGxN8i_H";
 
 // GPRS credentials
 char apn[]  = "airtelgprs.com";
@@ -23,19 +18,15 @@ char pass[] = "";
 // Define the phone number to which the alert message is to be send
 String PhNo = "+919400056322";
 
-// Software Serial for the GSM module
 #include <SoftwareSerial.h>
 SoftwareSerial SerialAT(3, 2); // RX, TX
 
-// Intialize the GSM library
 TinyGsm modem(SerialAT);
-// Define the Blynk map widget
 WidgetMap mymap(V0);
 
-// Centre coordinates of the jail
 float CLAT = 8.489536530804937;
 float CLNG = 76.96886048251929;
-// Some test coordinates
+
 float GPS_LOCATION_LAT[] =  { 8.490088316878953,
                               8.488618654136234,
                               8.490995578815479
@@ -45,40 +36,33 @@ float GPS_LOCATION_LONG[] = { 76.96865663463306,
                               76.96963832313622
   };
   
-// variables for program to function
 int key = 0;
 long int GPSprevMillis = 0;
 
-//Define the inmate ID to who the tracker belongs to
 const int inmateID = 1;
-// Define the in-built LED pin for the E-Tracker module
 const int statusLED = 6;
-//Define the pins to which the accelerometers are connected
+
 #define Xpin A1
 #define Ypin A2
 #define Zpin A3
 
-// Define the GPS interval
+
 const int GPSintv = 10000;
 
-// Define the perimeter radius
+
 const int radiusThresh = 2.5;
-// Define accelerometer threshold
+
 const int accThresh = 650;
 
-// Variables needed for GCD calculation
 const double DE2RA = 0.01745329252;
 const double AVG_ERAD = 6371.0;
 
 
-// variables used for latittude and longitude values
 float lat = 0;
 float lng = 0;
 
-//variables for storing accelerometer values
 int X= 0, Y = 0, Z= 0;
 
-// Send the GPS coordinates the blynk app
 void sendGPS(float lati, float lngi)
 {
   Blynk.virtualWrite(V2, lati);
@@ -87,56 +71,46 @@ void sendGPS(float lati, float lngi)
   mymap.location(index, lati, lngi, String("Here!"));
 }
 
-// get the appropriate GPS coordinates
 void getGPS()
 {
   if (key == -1)
   {       
-    // Get the live GPS coordinates
     modem.getGPS(&lat, &lng);
   }
   else
   {       
-    // Use one of the test coordinates
     lat = GPS_LOCATION_LAT[key];
     lng = GPS_LOCATION_LONG[key];
   } 
 }
 
-// Function to run once the device connects to blynk
 BLYNK_CONNECTED() {
-  //Turn the status led ON once connected to blynk server
   digitalWrite(statusLED,HIGH);
 }
 
 void setup()
 {
-  // Set the status LED as Output
   pinMode(statusLED, OUTPUT);
-  // Set the acc pins as input
   pinMode(Xpin, INPUT);
   pinMode(Ypin, INPUT);
   pinMode(Zpin, INPUT);
-  // Debug console
+  
   Serial.begin(9600);
   delay(10);
 
-  // Set GSM module baud rate
   SerialAT.begin(9600);
   delay(3000);
 
-  //Intializing GSM Modem
   Serial.println("Initializing modem...");
   modem.restart();
   modem.enableGPS();
   delay(1500);
 
-  // Connect to Blynk
+
   Blynk.begin(auth, modem, apn, user, pass);
   GPSprevMillis = millis();
 }
 
-// Function to calculate the Great Circle Distance between two points
 double GCDistance(double lat1, double lon1, double lat2, double lon2)
 {
    lat1 *= DE2RA;
@@ -150,7 +124,7 @@ double GCDistance(double lat1, double lon1, double lat2, double lon2)
 void loop()
 { 
   Blynk.run();
-
+  
   X = analogRead(Xpin);
   Y = analogRead(Ypin);
   Z = analogRead(Zpin);
@@ -162,29 +136,24 @@ void loop()
   Serial.print("Z : ");
   Serial.println(Z);
 
-  // check whether accelerometer had any trigger
   if ((X > accThresh) || (Y > accThresh) || (Z > accThresh))
   {
     Serial.println("Jail Break Attempt"); 
     Blynk.notify("Jail Break Attempt by " + String(inmateID));
    }
-   
-  //Check whether the time since last trigger reached set GPS interval
+     
   if ((millis() - GPSprevMillis) >= GPSintv)
   {
-    // Get the GPS Coordinates
     getGPS();
     Serial.print("KEY: " + String(key));
     Serial.println(" LAT: " + String(lat) + " LNG: " + String(lng));
-    // Send the obtained coordinates to Blynk
+
     sendGPS(lat,lng);
     if ((lat != 0) && (lng != 0))
     {
-      // Find the current radius
       double radius = GCDistance(CLAT, CLNG, lat, lng);
       Serial.println("Distance : " + String(radius));
 
-      // Check whether the test coordinated exceedes the set radius threshold
       if ((radius > radiusThresh) && (key != -1))
       {
         Serial.println("Jailbreak detected!!!");
@@ -201,7 +170,7 @@ void loop()
       } 
     }
     
-    key = (key == 2) || (key == -1) ? -1 : key +1;
+    key = (key == 2) || (key == -1) ? key=-1 : key +1;
     GPSprevMillis = millis();
   }
 }
